f0bd51966129ddadb6e3c61a02a6fe26b49eb25b
R15Hi <rk.8969009@gmail.com>
2024-07-15 10:20:26 +0530

    RMX1851: Add IMS libs symlinks

--- RMX1851-vendor.mk
+++ RMX1851-vendor.mk
@@ -1381,3 +1381,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-service-qti \
     vendor.qti.gnss@4.0-service \
     manifest_android.hardware.drm@1.3-service.widevine
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64
